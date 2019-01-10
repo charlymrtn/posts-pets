d586ae204dcd897c23078c4f7528f273cfa42765
Carlos Martin <cjmar87@gmail.com>
2019-01-10 18:21:44 +0000

    cambios en la vista

--- resources/views/sections/blog.blade.php
+++ resources/views/sections/blog.blade.php
@@ -5,7 +5,7 @@
             <div class="col-lg-3 col-md-4 col-xs-6">
 
                 <div class="single-blog">
-                    <div class="image"><img src="{{asset('images/'.$post->image)}}" alt=""></div>
+                    <div class="image"><img src="{{voyager::image($post->image)}}" alt=""></div>
                     <div class="text">
                         <h6>{{$post->author->name}}</h6>
                         <h5><a href="#!" class="tran3s">{{$post->title}}</a></h5>
